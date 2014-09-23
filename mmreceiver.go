d1c8345693e2dc8f418c56759105eda739cdb426
huzorro <huzorro@gmail.com>
2014-09-23 15:00:24 +0800

    modified host

--- mmreceiver.go
+++ mmreceiver.go
@@ -131,7 +131,7 @@ func postMessage(w http.ResponseWriter) {
 		http.Error(w, "xml Marshal failed", http.StatusBadRequest)
 		return
 	}
-	reply, err := postRequest("http://42.62.0.188:10087/mmReceiver", data)
+	reply, err := postRequest("http://:10087/mmReceiver", data)
 	log.Printf("receive response:", string(reply))
 	if err != nil {
 		http.Error(w, "post request failed", http.StatusBadRequest)
